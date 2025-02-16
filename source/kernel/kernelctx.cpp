b79f1eb834a7e91a860c2734896d3bbeccfce325
Correia <gabriel_correia0202@outlook.com>
2025-02-16 09:47:41 -0300

    (Kernel), Is the std::distance algorithm sufficient?

--- source/kernel/kernelctx.cpp
+++ source/kernel/kernelctx.cpp
@@ -1,24 +1,25 @@
 #include <cassert>
 #include <mutex>
+#include <ranges>
 #include <vector>
 
 #include <kernel/kernelctx.h>
 FastNx::I32 FastNx::Kernel::KernelContext::GetPid(const U64 prnde) {
-    constexpr auto MaximumProcessId{300};
+    constexpr auto MaximumProcessIds{300};
     static const I32 nxBasePid{processId};
-    static std::vector<std::pair<I32, U64>> pids(MaximumProcessId); // Let's search for the PID or create one from scratch
+    static std::vector<std::pair<I32, U64>> pids(MaximumProcessIds); // Let's search for the PID or create one from scratch
 
     I32 pid{};
     std::scoped_lock lock(_idsMutex);
-    for (I32 _itPid{}; _itPid != pids.size(); _itPid++) {
-        if (pids[_itPid].second == prnde) // Search only, immediate return
-            return pids[_itPid].first;
-        if (pids[_itPid].first == 0)
-            if ((pids[_itPid].second = prnde))
-                pid = processId = nxBasePid + _itPid;
-        assert(pids[_itPid].second > 0);
+    for (auto _itPid{std::begin(pids)}; _itPid != std::end(pids); ++_itPid) {
+        if (_itPid->second == prnde) // Search only, immediate return
+            return _itPid->first;
+        if (_itPid->first == 0)
+            if ((_itPid->second = prnde))
+                pid = processId = nxBasePid + std::distance(std::begin(pids), _itPid);
+        assert(_itPid->second > 0);
         if (pid)
-            if ((pids[_itPid].second = prnde))
+            if ((_itPid->first = pid))
                 break;
     }
     assert(pid);
