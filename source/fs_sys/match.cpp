03d6d35e563949128b97970c18d1aee48007252d
Correia <gabriel_correia0202@outlook.com>
2025-03-22 13:09:06 -0300

    (FsSys), MatchFiles now works with very large files

--- source/fs_sys/match.cpp
+++ source/fs_sys/match.cpp
@@ -4,17 +4,24 @@
 
 namespace FastNx::FsSys {
     bool MatchFiles(const VfsBackingFilePtr &first, const VfsBackingFilePtr &second) {
+        // Simple and cheap test
         if (first->GetSize() != second->GetSize())
             return {};
         if (first->Read<U64>() != second->Read<U64>())
             return {};
-        std::vector<U8> buffer(first->GetSize());
+        const auto flatsize{first->GetSize()};
+        std::vector<U8> buffer(std::min(flatsize, 128_KBYTES));
 
+        // The heavy part comes here
         std::array<Crypto::Checksum, 2> checkers;
         for (U64 offset{}; offset < first->GetSize(); ) {
+            if (const auto bufsz{std::min(buffer.size(), flatsize - offset)})
+                if (bufsz != buffer.size())
+                    buffer.resize(bufsz);
             const auto size{first->ReadSome(std::span(buffer), offset)};
             if (size)
                 checkers.front().Update(buffer);
+            else break;
             if (second->ReadSome(std::span(buffer), offset) == size)
                 checkers.back().Update(buffer);
 
